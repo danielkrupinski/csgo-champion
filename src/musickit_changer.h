a38c78bae8de7efccd8d4c713a69e4081dce602e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-31 15:22:12 +0200

    Add private variables in class MusicKitChanger

--- src/musickit_changer.h
+++ src/musickit_changer.h
@@ -6,6 +6,11 @@
 class MusicKitChanger {
 public:
     MusicKitChanger(int MusicID, remote::Handle* csgo, remote::MapModuleMemoryRegion* client);
+private:
+    unsigned long localPlayer = 0;
+    unsigned int LocalPlayerIndex;
+    unsigned int originalMusicID;
+    unsigned int spoofedMusicID = MusicID;
 };
 
 #endif // MUSICKIT_CHANGER_H
