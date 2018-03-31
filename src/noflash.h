d1b01a70b9688c9e15ed8baaa9801fd1705bf677
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-31 14:06:23 +0200

    Add header guard

--- src/noflash.h
+++ src/noflash.h
@@ -1,6 +1,11 @@
+#ifndef NOFLASH_H
+#define NOFLASH_H
+
 #include "cheat.h"
 
 class NoFlash {
 public:
     NoFlash(remote::Handle* csgo, remote::MapModuleMemoryRegion* client);
 };
+
+#endif // NOFLASH_H
