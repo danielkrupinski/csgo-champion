592bf53838cf09f40b695899a52446a3e54caf09
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-31 15:14:00 +0200

    Move musickit changer

--- src/cheat.cpp
+++ src/cheat.cpp
@@ -251,47 +251,6 @@ void cheat::RCS(float sensitivity, float m_yaw, float m_pitch, Vector2D rcsValue
 	RCSLastPunch = vecPunch;
 }
 
-void cheat::SpoofMusicKit(int MusicID, remote::Handle* csgo, remote::MapModuleMemoryRegion* client)
-{
-	if(!csgo || !client)
-		return;
-
-	if(!csgo->MusicKitChangerEnabled)
-		return;
-
-	unsigned long localPlayer = 0;
-	unsigned int LocalPlayerIndex;
-	unsigned int originalMusicID;
-	unsigned int spoofedMusicID = MusicID;
-
-	csgo->Read((void*) csgo->m_addressOfLocalPlayer, &localPlayer, sizeof(long)); // TODO: GetLocalPlayerIndex from engine_client.so
-
-	if(!localPlayer)
-		return;
-
-	csgo->Read((void*) (localPlayer+0x94), &LocalPlayerIndex, sizeof(LocalPlayerIndex));
-
-	if(!LocalPlayerIndex)
-		return;
-
-	if(LocalPlayerIndex > 64) // Not a correct value.
-		return;
-
-	csgo->Read((void*) (csgo->PlayerResourcesPointer), &csgo->m_addressOfPlayerResource, sizeof(unsigned long));
-
-	csgo->Read((void*) (csgo->m_addressOfPlayerResource + 0x5020 + (LocalPlayerIndex * 4)), &originalMusicID, sizeof(originalMusicID));
-
-	if(!originalMusicID)
-		return;
-
-	if(csgo->m_addressOfPlayerResource && originalMusicID != spoofedMusicID)
-	{
-		csgo->Write((void*) (csgo->m_addressOfPlayerResource + 0x5020 + (LocalPlayerIndex * 4)), &spoofedMusicID, sizeof(spoofedMusicID));
-		cout << "Changed music kit ID to " << dec << spoofedMusicID << " on address " << hex << csgo->m_addressOfPlayerResource + 0x5020 + (LocalPlayerIndex * 4);
-		cout << " on entity index " << dec << LocalPlayerIndex << endl;
-	}
-}
-
 void cheat::FovChanger(int fov, remote::Handle* csgo, remote::MapModuleMemoryRegion* client)
 {
 	if(!csgo || !client)
