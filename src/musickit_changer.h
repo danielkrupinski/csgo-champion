3ecf011241c460df8dd31ee3b68261ed41c30146
Daniel Krupiński <danielkrupinski@outlook.com>
2018-03-31 15:23:03 +0200

    Add default value for member variables

--- src/musickit_changer.h
+++ src/musickit_changer.h
@@ -7,10 +7,10 @@ class MusicKitChanger {
 public:
     MusicKitChanger(int MusicID, remote::Handle* csgo, remote::MapModuleMemoryRegion* client);
 private:
-    unsigned long localPlayer = 0;
-    unsigned int LocalPlayerIndex;
-    unsigned int originalMusicID;
-    unsigned int spoofedMusicID = MusicID;
+    unsigned long localPlayer {0};
+    unsigned int LocalPlayerIndex {0};
+    unsigned int originalMusicID {0};
+    unsigned int spoofedMusicID {0}; //MusicID;
 };
 
 #endif // MUSICKIT_CHANGER_H
